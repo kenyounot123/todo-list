fa8b3e88ba9764b195e2135ef32b372532b3e477
kenyounot123 <kenlu519@gmail.com>
2024-02-21 21:50:12 -0500

    Create todo list layout in frontend

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
-// Import our custom CSS
-import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+// Import our custom CSS
+import '../scss/styles.scss'
+
+import { Todo } from './todoItems.js'
+
+const item = new Todo('title', 'title', 'title', 'title');
+console.log(item)
+console.log(item.description) 
---
+++ src/js/todoItems.js
@@ -0,0 +1,8 @@
+export class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = 'gwegweg' 
+    this.description = 'gwegweg' 
+    this.dueDate = 'gwegweg' 
+    this.priority = 'gwegweg' 
+  }
+}
